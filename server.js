bbb644d3cf64276334410f09d3a5d889b98f019e
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-04-24 19:48:27 -0700

    linked server.js to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
-//Default response for any other request (Not Found)
+// Connect to Database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user:'root',
+    password: 'will1995',
+    database: 'election'
+  },
+  console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
